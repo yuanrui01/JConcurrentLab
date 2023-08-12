96ba180c9dbbcfd6e9a39ad0fb6d1ffdd9d0ee78
yuanrui01 <xdyrfree@gmail.com>
2023-08-12 23:11:27 +0800

    初始化

---
+++ src/main/java/com/foresee/concurrent/Main.java
@@ -0,0 +1,13 @@
+package com.foresee.concurrent;
+
+/**
+ * @description:
+ * @author: yuanrui
+ * @email: xdyrfree@gmail.com
+ * @date: 2023/8/12-23:08
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
