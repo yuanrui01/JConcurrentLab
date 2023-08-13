05d5c6ca4196680d3431ac5cf7ba840451978bd3
yuanrui01 <xdyrfree@gmail.com>
2023-08-13 11:16:58 +0800

    可见性问题验证

---
+++ src/main/java/com/foresee/concurrent/chapter3/MutableInteger.java
@@ -0,0 +1,20 @@
+package main.java.com.foresee.concurrent.chapter3;
+
+/**
+ * @description: 可变整数
+ * @author: yuanrui
+ * @email: xdyrfree@gmail.com
+ * @date: 2023/8/13-2:37
+ */
+public class MutableInteger {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
