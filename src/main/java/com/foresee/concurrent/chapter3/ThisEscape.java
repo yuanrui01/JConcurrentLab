3f872c676f6d01113eaeeb0bb58de793a39a767d
yuanrui01 <xdyrfree@gmail.com>
2023-08-13 11:30:37 +0800

    发布与逃逸问题示例

---
+++ src/main/java/com/foresee/concurrent/chapter3/ThisEscape.java
@@ -0,0 +1,33 @@
+package main.java.com.foresee.concurrent.chapter3;
+
+import java.awt.*;
+import java.util.EventListener;
+
+/**
+ * @description:
+ * @author: yuanrui
+ * @email: xdyrfree@gmail.com
+ * @date: 2023/8/13-11:19
+ */
+public class ThisEscape {
+    public ThisEscape(EventSource source) {
+        source.registerListener(
+                new EventListener() {
+                    public void onEvent(Event e) {
+                        ThisEscape escape = doSomething();
+                        // 在构造函数中访问ThisEscape对象的引用
+                        escape.doAnotherThing();
+                    }
+                }
+        );
+    }
+
+    public ThisEscape doSomething() {
+        // 执行操作...
+        return this;
+    }
+
+    public void doAnotherThing() {
+        // 执行其他操作...
+    }
+}
