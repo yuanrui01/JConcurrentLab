096f82434f104dd1db5c7e79f850912ea07cf520
yuanrui01 <xdyrfree@gmail.com>
2023-09-26 21:24:28 +0800

    test

--- src/main/java/com/foresee/CompletableFutureUseDemo.java
+++ src/main/java/com/foresee/CompletableFutureUseDemo.java
@@ -1,9 +1,6 @@
 package com.foresee;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -14,31 +11,40 @@ public class CompletableFutureUseDemo {
 
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
         AtomicInteger ans = new AtomicInteger(-1);
         AtomicReference<String> haha = new AtomicReference<>("dsldjjdas");
-        CompletableFuture.supplyAsync(() -> {
-            int i = ThreadLocalRandom.current().nextInt(10);
-            try {
-                TimeUnit.MILLISECONDS.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                int i = ThreadLocalRandom.current().nextInt(10);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-            System.out.println("1秒钟后出结果 ------ result:" + i);
-            ans.set(i);
-            haha.set("34231");
-            return i;
-        }).whenComplete((result, e) -> {
-            if (e == null)
-                System.out.println("成功出结果了！！！result: " + result);
-        }).exceptionally(e -> {
-            System.out.println("唉，发生异常了~~~~~~");
-            return 0;
-        });
+                System.out.println("1秒钟后出结果 ------ result:" + i);
+                ans.set(i);
+                haha.set("34231");
+                return i;
+            }, threadPool).whenComplete((result, e) -> {
+                if (e == null)
+                    System.out.println("成功出结果了！！！result: " + result);
 
-        while ( ans.get() == -1) {}
+                System.out.println(Thread.currentThread().getName());
+            }).exceptionally(e -> {
+                System.out.println("唉，发生异常了~~~~~~");
+                return 0;
+            });
+        } finally {
+            threadPool.shutdown();
+            System.out.println(Thread.currentThread().getName());
+        }
 
-        System.out.println(haha);
+//        while ( ans.get() == -1) {}
+//
+//        System.out.println(haha);
     }
 
 
